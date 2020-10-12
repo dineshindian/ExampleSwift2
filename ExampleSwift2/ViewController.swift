aa08393f43f4afa8e097db603ae76d26adc2ac18
dineshindian <64672817+dineshindian@users.noreply.github.com>
2020-10-12 09:41:34 +0530

    Initial Commit

---
+++ ExampleSwift2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ExampleSwift2
+//
+//  Created by Dinesh Gurubaran on 12/10/20.
+//  Copyright © 2020 Dinesh Gurubaran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
